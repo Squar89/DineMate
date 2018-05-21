d683d2301fba673a5ff50ed8f435121128d31650
Piotr Olczak <po385891@students.mimuw.edu.pl>
2018-05-21 13:57:33 +0200

    Zmiana pól tabeli dishes w bazie danych

--- CREATE.sql
+++ CREATE.sql
@@ -1,31 +1,47 @@
-CREATE TABLE IF NOT EXISTS users
+create table users
 (
-  user_id INTEGER PRIMARY KEY,
-  login VARCHAR(30) UNIQUE NOT NULL,
-  password_hash VARCHAR(30) NOT NULL,
-  date_of_registration TIMESTAMP NOT NULL
+  user_id              integer     not null
+    constraint users_pkey
+    primary key,
+  login                varchar(30) not null
+    constraint users_login_key
+    unique,
+  password_hash        varchar(30) not null,
+  date_of_registration timestamp   not null
 );
 
-CREATE TABLE IF NOT EXISTS loggings
+create table loggings
 (
-  user_id INTEGER REFERENCES users,
-  date TIMESTAMP NOT NULL
+  user_id integer
+    constraint loggings_user_id_fkey
+    references users,
+  date    timestamp not null
 );
 
-CREATE TABLE IF NOT EXISTS dishes
+create table dishes
 (
-  dish_id INTEGER PRIMARY KEY,
-  description TEXT,
-  name  VARCHAR(30),
-  ingredients_list VARCHAR(30),
-  directions VARCHAR(30)
+  dish_id     integer not null
+    constraint dishes_pkey
+    primary key,
+  name        text,
+  ingredients text,
+  directions  text,
+  image_url   text
 );
 
-CREATE TABLE IF NOT EXISTS ratings
+create table ratings
 (
-  user_id INTEGER REFERENCES users,
-  dish_id INTEGER REFERENCES dishes,
-  rate NUMERIC CHECK (rate > 0 AND rate < 6), --TODO
-  date TIMESTAMP NOT NULL,
-  PRIMARY KEY (user_id, dish_id)
+  user_id integer   not null
+    constraint ratings_user_id_fkey
+    references users,
+  dish_id integer   not null
+    constraint ratings_dish_id_fkey
+    references dishes,
+  rate    numeric
+    constraint ratings_rate_check
+    check ((rate > (0) :: numeric) AND (rate < (6) :: numeric)),
+  date    timestamp not null,
+  constraint ratings_pkey
+  primary key (user_id, dish_id)
 );
+
